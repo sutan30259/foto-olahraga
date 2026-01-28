6012e6a31eb282073ed5a6ccc5aa40aeee27e5d6
sutan30259 <suts30259@gmail.com>
2026-01-28 20:53:59 +0700

    Setup: Migrations untuk paket, pesanan, dan foto

---
+++ database/migrations/2026_01_28_134303_create_pesanan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pesanan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(); //id klient
+            $table->foreignId('paket_id')->constrained(); //id paket
+            $table->string('kode_pesanan')->unique(); //contoh <ODR-20260128-001
+            $table->date('tanggal_foto');
+            $table->string('status_bayar')->default('belum_bayar'); //belum bayar, dp, lunas
+            $table->string('metode_bayar')->nullable(); //midtrans, cash
+            $table->integer('total_harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pesanans');
+    }
+};
---
+++ database/migrations/2026_01_28_134749_create_fotos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fotos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pesanan_id')->constrained()->onDelete('cascade');
+            $table->string('path_foto'); //Alamat file foto di storage
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fotos');
+    }
+};
